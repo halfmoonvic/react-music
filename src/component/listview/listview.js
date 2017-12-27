51a39b778e5280eadf9f6316e96a5893443ad63f
cs-300 <halfmoonvic@gmail.com>
2017-12-27 10:00:07 +0800

    feat(fixTitle): 固定标题向上顶

--- src/component/listview/listview.js
+++ src/component/listview/listview.js
@@ -12,6 +12,7 @@ import Scroll from 'component/scroll/scroll'
 /**** 当前组件的 子组件等 ***/
 
 const ANCHOR_HEIGHT = 20
+const TITLE_HEIGHT = 30
 
 class ListView extends Component {
   static defaultProps = {
@@ -23,7 +24,8 @@ class ListView extends Component {
       shortcutList: [],
       scrollY: -1,
       currentIndex: 0,
-      fixedTitle: '热门'
+      fixedTitle: '热门',
+      diff: -1
     }
 
     this.touch = {}
@@ -35,6 +37,7 @@ class ListView extends Component {
     this.scroll = this.scroll.bind(this)
     this._calculateHeight = this._calculateHeight.bind(this)
     this._fixTitle = this._fixTitle.bind(this)
+    this._diffTitle = this._diffTitle.bind(this)
   }
   componentWillReceiveProps(nextProps) {
     if (nextProps.data.length) {
@@ -49,12 +52,16 @@ class ListView extends Component {
       setTimeout(() => { this._calculateHeight() }, 20)
     }
     if (this.state.scrollY !== nextState.scrollY) {
-      const newY = nextState.scrollY
-      this._hightLightCurrent(newY)
+      // const newY = nextState.scrollY
+      this._hightLightCurrent(nextProps, nextState)
     }
 
     if (this.state.currentIndex !== nextState.currentIndex) {
       this._fixTitle(nextProps, nextState)
+    }
+
+    if (this.state.diff !== nextState.diff) {
+      this._diffTitle(nextProps, nextState)
     }
 
     return true
@@ -86,31 +93,6 @@ class ListView extends Component {
 
     forceCheck()
   }
-  _fixTitle(nextProps, nextState) {
-    this.setState({
-      fixedTitle: nextProps.data[nextState.currentIndex] ? nextProps.data[nextState.currentIndex].title : '热门'
-    })
-  }
-  _hightLightCurrent(pos) {
-    const listHeight = this.listHeight
-    // 当滚动到顶部， pos > 0
-    if (pos > 0) {
-      this.setState({ currentIndex: 0 })
-      return
-    }
-    // 在中间部分滚动
-    for (let i = 0; i < listHeight.length - 1; i++) {
-      let height1 = listHeight[i]
-      let height2 = listHeight[i + 1]
-      if (!height2 || (-pos >= height1 && -pos < height2)) {
-        this.setState({ currentIndex: i })
-        return
-      }
-    }
-
-    // 在底部，且 -pos 大于最后一个元素的上限 第一个减1 是因为 listHeight 多一个 item 的，第二个减一 是因为， currentIndex 是从0 开始的
-    this.setState({ currentIndex: listHeight.length - 1 - 1 })
-  }
   _scrollTo(index) {
     if (!index && index !== 0) {
       return
@@ -137,6 +119,42 @@ class ListView extends Component {
       height += item.clientHeight
       this.listHeight.push(height)
     }
+  }
+  _fixTitle(nextProps, nextState) {
+    this.setState({
+      fixedTitle: nextProps.data[nextState.currentIndex] ? nextProps.data[nextState.currentIndex].title : '热门'
+    })
+  }
+  _hightLightCurrent(nextProps, nextState) {
+    const newY = nextState.scrollY
+    const listHeight = this.listHeight
+    // 当滚动到顶部， newY > 0
+    if (newY > 0) {
+      this.setState({ currentIndex: 0 })
+      return
+    }
+    // 在中间部分滚动
+    for (let i = 0; i < listHeight.length - 1; i++) {
+      let height1 = listHeight[i]
+      let height2 = listHeight[i + 1]
+      if (!height2 || (-newY >= height1 && -newY < height2)) {
+        this.setState({ diff: height2 + newY })
+        this.setState({ currentIndex: i })
+        return
+      }
+    }
+
+    // 在底部，且 -newY 大于最后一个元素的上限 第一个减1 是因为 listHeight 多一个 item 的，第二个减一 是因为， currentIndex 是从0 开始的
+    this.setState({ currentIndex: listHeight.length - 1 - 1 })
+  }
+  _diffTitle(nextProps, nextState) {
+    const { diff } = nextState
+    let fixedTop = (diff > 0 && diff < TITLE_HEIGHT) ? diff - TITLE_HEIGHT : 0
+    if (this.fixedTop === fixedTop) {
+      return
+    }
+    this.fixedTop = fixedTop
+    this.refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`
   }
   render() {
     return (
@@ -171,8 +189,8 @@ class ListView extends Component {
             <li className={classNames('shortcutList__item', {'shortcutList__item--current': i === this.state.currentIndex})} data-index={i} key={v}>{v}</li>
           )) : null}</ul>
         </div>
-        <div className="list-fixed">
-          {this.state.fixedTitle !== '热门' ? <h1 className="fixed-title">{this.state.fixedTitle}</h1> : null}
+        <div className="list-fixed" ref="fixed">
+          {this.state.scrollY < 0 ? <h1 className="fixed-title">{this.state.fixedTitle}</h1> : null}
         </div>
         {!this.props.data.length ? <div className="loading-container">
           <Loading></Loading>
